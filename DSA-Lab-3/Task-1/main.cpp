54666063088ad881506d545747466a7dc7cd3978
Zia Ul Rehman <166900840+namher-sec@users.noreply.github.com>
2024-10-27 12:44:47 +0500

    Update main.cpp

--- DSA-Lab-3/Task-1/main.cpp
+++ DSA-Lab-3/Task-1/main.cpp
@@ -2,21 +2,30 @@
 #include "Stack.h"
 
 int main() {
-    Stack stack;
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
+    Stack stack1;
+    stack1.push(10);
+    stack1.push(20);
+    stack1.push(30);
 
-    std::cout << "Top element is: " << stack.peek() << std::endl;
+    std::cout << "Top element is: " << stack1.peek() << std::endl;
 
-    std::cout << "Popped element: " << stack.pop() << std::endl;
-    std::cout << "Top element after pop: " << stack.peek() << std::endl;
+    std::cout << "Popped element: " << stack1.pop() << std::endl;
+    std::cout << "Top element after pop: " << stack1.peek() << std::endl;
 
-    while (!stack.is_empty()) {
-        std::cout << "Popped element: " << stack.pop() << std::endl;
+    std::cout << "All elements in Stack: ";
+    stack1.display();
+
+    while (!stack1.is_empty()) {
+        std::cout << "Popped element: " << stack1.pop() << std::endl;
     }
 
-    stack.pop(); // This will indicate underflow
+    stack1.pop(); // This will indicate underflow
+
+    stack1.push(50);
+    stack1.push(60);
+    stack1.push(70);
+
+    stack1.display();
 
     return 0;
 }
